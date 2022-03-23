52649d967e89865b77aabc2b8c57cad662013742
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-23 11:37:38 -0700

    created home page

---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Welcome To Our Home Page
+        <p>Feel free to checkout out our Library</p>
+      </h1>{' '}
+    </div>
+  );
+}
